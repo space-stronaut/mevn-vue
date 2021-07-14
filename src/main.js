3774d8e3dc13cb1b41604731a0204d85406336fd
space-stronaut <abelr6099@gmail.com>
2021-07-14 18:20:41 +0700

    vue mevn commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import vuetify from './plugins/vuetify'
+import store from './store'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.use(axios)
+
 
 new Vue({
-  render: h => h(App),
+  router,
+  vuetify,
+  store,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul v-for="user in users" :key="user.id">
+      <li>Nama : {{ user.name }}</li>
+      <li>umur : {{ user.age }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: 'Home',
+    data(){
+      return {
+        users : []
+      }
+    },
+    mounted(){
+      this.getUsers()
+    },
+    methods : {
+      async getUsers(){
+        let response = await axios.get('http://127.0.0.1:3000/users')
+
+        this.users = response.data.result
+      }
+    },
+  }
+</script>
