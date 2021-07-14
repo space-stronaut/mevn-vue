d02260de2009f8847dee85e3ff64a888a666d4d5
space-stronaut <abelr6099@gmail.com>
2021-07-15 06:50:15 +0700

    add get/post/del

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,41 @@
     <ul v-for="user in users" :key="user.id">
       <li>Nama : {{ user.name }}</li>
       <li>umur : {{ user.age }}</li>
+      <li>
+        <v-btn  
+        color="danger"
+        @click="handleDelete(user.name)"
+        >
+          Delete
+        </v-btn>
+      </li>
     </ul>
+
+    <v-form
+      @submit.prevent="handleSubmit"
+    >
+      <v-col>
+        <v-text-field
+          v-model="forms.name"
+        >
+        </v-text-field>
+      </v-col>
+      <v-col>
+        <v-text-field
+          type="number"
+          v-model="forms.age"
+        >
+        </v-text-field>
+      </v-col>
+      <v-col>
+        <v-btn
+          type="submit"
+          color="primary"
+          block
+        >Submit
+        </v-btn>
+      </v-col>
+    </v-form>
   </div>
 </template>
 
@@ -14,7 +48,11 @@
     name: 'Home',
     data(){
       return {
-        users : []
+        users : [],
+        forms : {
+          name : null,
+          age : null
+        }
       }
     },
     mounted(){
@@ -25,6 +63,32 @@
         let response = await axios.get('http://127.0.0.1:3000/users')
 
         this.users = response.data.result
+      },
+      async handleSubmit(){
+        try{
+          let response = await axios.post('http://127.0.0.1:3000/users', this.forms)
+
+          if (response.status === 200) {
+            console.log(response)
+            this.getUsers()
+            this.forms.name = null
+            this.forms.age = null
+          }
+        }catch(err){
+          alert(err)
+        }
+      },
+      async handleDelete(e){
+        try{
+          let response = await axios.get('http://127.0.0.1:3000/delete/' + e)
+
+          if (response.status === 200) {
+            console.log(response)
+            this.getUsers()
+          }
+        }catch(err){
+          alert(err)
+        }
       }
     },
   }
